5a73a4294f380c7578f9292bc1c403a0b9df1b47
Aysham <aysham@mail.example.com>
2023-06-07 16:37:29 +0200

    Fixed router for now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
  
 import './App.scss';
 import {MemoizedMovieList as Movies} from './components/MovieList';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import MovieTrailer from './pages/MovieTrailer';
 
 
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className="App"> 
       <Routes>
-          <Route path='/' element={<Movies/>}/>
-          <Route exact path='/:id' element={<MovieTrailer/>}/> 
-      </Routes>
+          <Route path='*' element={<Movies/>}/>
+           
+      </Routes> 
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client";
 import Header from './components/Header';
 
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 
 const client = new ApolloClient({
